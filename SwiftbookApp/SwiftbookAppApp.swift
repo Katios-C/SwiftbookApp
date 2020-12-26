f351ac14e73a9b728625fe0434a784d8c4024065
Katios <kate.chonkina@gmail.com>
2020-12-26 10:25:34 +0600

    Initial Commit

---
+++ SwiftbookApp/SwiftbookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftbookAppApp.swift
+//  SwiftbookApp
+//
+//  Created by Екатерина Чернова on 26.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftbookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
